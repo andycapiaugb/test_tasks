744fc69d684720ca3a89894b1a5d0e1273bc3065
Andy Capiau <andy.capiau@geraardsbergen.be>
2011-05-17 08:43:29 +0100

    Corrected bug in users_controller.rb: in sql statement, had another "==" that needed to be "="

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
     if current_user
       @user = User.find(params[:id])
       @requested_tasks = @user.taskrequests
-      @todos = TaskrequestsStep.joins(:step, :taskrequest).order("id DESC").where("current = ? AND ((steps.self = ? AND taskrequests.user_id == ?) OR executer_id == ?)", true, true, current_user.id, current_user.id)
+      @todos = TaskrequestsStep.joins(:step, :taskrequest).order("id DESC").where("current = ? AND ((steps.self = ? AND taskrequests.user_id = ?) OR executer_id = ?)", true, true, current_user.id, current_user.id)
     else
       redirect_to log_in_url, :notice => "Gelieve aan te melden"
     end
